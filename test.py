1b7a0e6cc1f66d101e84703ebf165512247df863
NikitaMulyar <ni***ol@mail.ru>
2023-03-04 02:18:07 +0300

    1,2,3,4,5,6 задачи

--- test.py
+++ test.py
@@ -1,10 +1,26 @@
 from requests import get, post
 
-"""print(get('http://127.0.0.1:8080/api/jobs').json())
+print(get('http://127.0.0.1:8080/api/jobs').json())
 print(get('http://127.0.0.1:8080/api/jobs/2').json())
 print(get('http://127.0.0.1:8080/api/jobs/342874343').json())
-print(get('http://127.0.0.1:8080/api/jobs/eee').json())"""
+print(get('http://127.0.0.1:8080/api/jobs/eee').json())
 print(post('http://127.0.0.1:8080/api/jobs',
            json={"id": 1, "team_leader_email": "1233@123", "title": "testing", "work_size": 16,
                  "hazard_level": 9, "collaborators": None,
-                  "start_date": None, "end_date": None, "is_finished": True}).text)
+                  "start_date": None, "end_date": None, "is_finished": True}).json())
+print(post('http://127.0.0.1:8080/api/jobs',
+           json={"id": 1, "team_leader_email": "1233@123", "title": "testing", "work_size": 16,
+                 "hazard_level": 9, "collaborators": None,
+                  "start_date": None, "end_date": None, "is_finished": True}).json())
+# Создаем дубликат с таким же id - ошибка
+print(post('http://127.0.0.1:8080/api/jobs',
+           json={"id": 1, "team_leader_email": "12333@123", "title": "testing", "work_size": 16,
+                 "hazard_level": 9, "collaborators": None,
+                  "start_date": None, "end_date": None, "is_finished": True}).json())
+# Нет юзера с такой почтой - ошибка
+print(post('http://127.0.0.1:8080/api/jobs',
+           json={"id": 1, "team_leader_email": "12333@123", "work_size": 16,
+                 "hazard_level": 9, "collaborators": None,
+                  "start_date": None, "end_date": None, "is_finished": True}).json())
+# Нет заголовка - ошибка
+print(get('http://127.0.0.1:8080/api/jobs').json())
