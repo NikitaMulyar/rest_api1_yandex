fa6449508398036b53f6e656290e42eb813902d0
NikitaMulyar <ni***ol@mail.ru>
2023-03-14 19:52:33 +0300

    ура, можно делать рестфул

--- main.py
+++ main.py
@@ -424,20 +424,22 @@ def not_found(_):
 
 @app.route('/users_show/<int:user_id>')
 def user_show(user_id):
-    info = get(f'http://127.0.0.1:8080/api/users/{user_id}').json()['user']
-    res = get(f'https://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={info["city_from"]}&format=json')
-    pos = ",".join(res.json()['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'].split())
-    map_params = {
-        "ll": pos,
-        "l": 'sat',
-        "z": '14'
-    }
-    map_api_server = "http://static-maps.yandex.ru/1.x/"
-    response = get(map_api_server, params=map_params)
-    map_file = f"static/img/map.png"
-    with open(map_file, mode="wb") as file:
-        file.write(response.content)
-    return render_template('nostalgy.html', data=info)
+    info = get(f'http://127.0.0.1:8080/api/users/{user_id}').json().get('user')
+    if info is not None:
+        res = get(f'https://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={info["city_from"]}&format=json')
+        pos = ",".join(res.json()['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'].split())
+        map_params = {
+            "ll": pos,
+            "l": 'sat',
+            "z": '14'
+        }
+        map_api_server = "http://static-maps.yandex.ru/1.x/"
+        response = get(map_api_server, params=map_params)
+        map_file = f"static/img/map.png"
+        with open(map_file, mode="wb") as file:
+            file.write(response.content)
+    f = info is None
+    return render_template('nostalgy.html', data=info, flag=f)
 
 
 if __name__ == '__main__':
